e763c09d02d60cfaa7544884c531177563daef41
ghtaoge <861038787@qq.com>
2019-04-21 01:03:01 +0800

    添加项目文件。

---
+++ VueExample/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VueExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Bind()
+        {
+            return View();
+        }
+
+        public ActionResult OnEvent()
+        {
+            return View();
+        }
+
+        public ActionResult Model()
+        {
+            return View();
+        }
+        public ActionResult IfElse()
+        {
+            return View();
+        }
+
+        public ActionResult Computed()
+        {
+            return View();
+        }
+
+        public ActionResult Component()
+        {
+            return View();
+        }
+
+        public ActionResult ComponentConfig()
+        {
+            return View();
+        }
+
+        public ActionResult ComponentArg()
+        {
+            return View();
+        }
+
+        public ActionResult Slots()
+        {
+            return View();
+        }
+
+        public ActionResult Mixins()
+        {
+            return View();
+        }
+
+        public ActionResult Directive()
+        {
+            return View();
+        }
+
+        public ActionResult Filter()
+        {
+            return View();
+        }
+
+        public ActionResult ComponentCF()
+        {
+            return View();
+        }
+
+        public ActionResult ComponentPX()
+        {
+            return View();
+        }
+    }
+}
